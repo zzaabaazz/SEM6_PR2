0d14e9e2889dee28d6fb3f9707081c59735bcdf8
zzaabaazz <58651910+zzaabaazz@users.noreply.github.com>
2024-04-03 16:48:19 +0300

    вроде готово, но я хз че происходит

--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -1,11 +1,10 @@
 #pragma once
-#define _USE_MATH_DEFINES
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
 
 #define N 500 // Число отрезков для вычисления плотности распределения вероятности 
-#define MX 100000L // Число экспериментов
+#define M 100000L // Число экспериментов
 //float sl_vel(float a, float b);
 float f[N];
 
@@ -46,14 +45,13 @@ namespace Project1 {
 	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
 	protected:
 
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -68,7 +66,6 @@ namespace Project1 {
 			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -107,19 +104,11 @@ namespace Project1 {
 			this->textBox1->Size = System::Drawing::Size(181, 20);
 			this->textBox1->TabIndex = 2;
 			// 
-			// textBox2
-			// 
-			this->textBox2->Location = System::Drawing::Point(72, 403);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(181, 20);
-			this->textBox2->TabIndex = 3;
-			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(725, 458);
-			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->chart1);
@@ -134,66 +123,86 @@ namespace Project1 {
 	private: System::Void chart1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+//help
+		float D = 1, a = 0.15, d;
+		float k1, k2, ea;
+		array<float>^ x = gcnew array<float>(N);
+		array<int>^ counts = gcnew array<int>(N);
 
-		srand(time(0));
-		for (int i = 0; i <= N; i++)
-		{
-			f[i] = 0;
+		k2 = exp(-a);
+		k1 = sqrt(D * (1.0 - k2 * k2));
 
+		x[0] = gauss(0, D);
+		for (int n = 1; n < N; n++) {
+			ea = gauss(0, 1);
+			x[n] = k1 * ea + k2 * x[n - 1];
 		}
 
-		long m; // Индекс для номера эксперимента
-		float x, a = 0, b = 7, d;
-		int n;
-		float ff = 0.0;
-		d = (b - a) / N; //величина отрезка случайной величины х
-
-		for (m = 0; m < MX; m++) // Цикл по числу экспериментов
-		{
-			x = sl_vel();
-			//textBox2->Text += x + "; \r\n";
-			n = (x - a) / (b - a) * N;
-			f[n] = f[n] + 1.;
+		// Find the minimum and maximum values in the x array
+		float minValue = x[0];
+		float maxValue = x[0];
+		for (int i = 1; i < N; i++) {
+			if (x[i] < minValue) {
+				minValue = x[i];
+			}
+			if (x[i] > maxValue) {
+				maxValue = x[i];
+			}
 		}
 
+		d = (maxValue - minValue) / N; // bin width
 
-		for (int i = 0; i <= 500; i++) textBox1->Text += f[i] + "; \r\n";
-
-		for (n = 1; n < N; n++) // Пересчет вероятности в плотность распределения вероятности
-		{
-			f[n] = f[n] / (MX * d);
-			chart1->Series[0]->Points->AddXY(n, f[n]);
+		// Initialize the counts array to zero
+		for (int i = 0; i < N; i++) {
+			counts[i] = 0;
 		}
 
+		// Generate M samples from the x array and count occurrences within each bin
+		for (int m = 0; m < M; m++) {
+			float sample = x[(int)(rand() % N)];
+			int n = (int)floor((sample - minValue) / d);
+
+			if (n >= 0 && n < N) {
+				counts[n]++;
+			}
+		}
+
+		// Convert counts to probability density
+		array<float>^ f = gcnew array<float>(N);
+		for (int n = 0; n < N; n++) {
+			f[n] = counts[n] / (float)(M * d);
+		}
+
+		// Plot the probability density
+		for (int n = 0; n < N; n++) {
+			chart1->Series[0]->Points->AddXY(minValue + n * d, f[n]);
+		}
 	}
-		   float sl_vel() {
-			   float u1, u2, x;
-			   for (int i = 0; i < MX; i++) {
-				   u1 = (float)rand() / RAND_MAX;
-				   u2 = (float)rand() / RAND_MAX;
-				   x = u1 * 7; // диапазон значений [0, 7)
 
-				   if (x < 3) {
-					   if (u2 <= 0.1) break;
-				   }
-				   else if (x >= 3 && x < 5) {
-					   if (u2 == 0) break; // вероятность равна 0, поэтому мы не можем сгенерировать значение в этом диапазоне
-				   }
-				   else {
-					   if (u2 <= -0.35 * x + 2.45) break;
-				   }
+		   float gauss(float mu, float sigma) {
+			   static float spare;
+			   static bool hasSpare = false;
+
+			   float ran;
+
+			   if (hasSpare) {
+				   hasSpare = false;
+				   return spare * sigma + mu;
 			   }
 
-			   return x;
-		   }
+			   float u, v, s;
+			   do {
+				   u = 2.0 * (float)rand() / RAND_MAX - 1.0;
+				   v = 2.0 * (float)rand() / RAND_MAX - 1.0;
+				   s = u * u + v * v;
+			   } while (s >= 1.0 || s == 0.0);
 
-		   int random(int x)
-		   {
-			   int r = rand() % x;
-			   return r;
+			   float fac = sqrt(-2.0 * log(s) / s);
+			   spare = v * fac;
+			   hasSpare = true;
+			   ran = u * fac;
 
+			   return ran * sigma + mu;
 		   }
 	};
 }
-
-
