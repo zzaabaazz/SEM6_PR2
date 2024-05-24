81e8e44b2dd9d3b40b51b96fb83517c3594337e9
stud034163 <stud034163@XO-MONO-417>
2024-05-24 10:22:18 +0300

    [ГОТОВО]

--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -45,8 +45,12 @@ namespace Project1 {
 		}
 	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::TextBox^ textBox1;
+
 	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart2;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart3;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart4;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
 	protected:
 
 	private:
@@ -68,12 +72,23 @@ namespace Project1 {
 			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
 			System::Windows::Forms::DataVisualization::Charting::Legend^ legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
 			System::Windows::Forms::DataVisualization::Charting::Series^ series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea3 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea4 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->chart2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->chart3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->chart4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart4))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// chart1
@@ -82,21 +97,21 @@ namespace Project1 {
 			this->chart1->ChartAreas->Add(chartArea1);
 			legend1->Name = L"Legend1";
 			this->chart1->Legends->Add(legend1);
-			this->chart1->Location = System::Drawing::Point(29, 2);
+			this->chart1->Location = System::Drawing::Point(29, 34);
 			this->chart1->Name = L"chart1";
 			series1->ChartArea = L"ChartArea1";
 			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
 			series1->Legend = L"Legend1";
 			series1->Name = L"Series1";
 			this->chart1->Series->Add(series1);
-			this->chart1->Size = System::Drawing::Size(658, 353);
+			this->chart1->Size = System::Drawing::Size(575, 287);
 			this->chart1->TabIndex = 0;
 			this->chart1->Text = L"chart1";
 			this->chart1->Click += gcnew System::EventHandler(this, &MyForm::chart1_Click);
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(612, 408);
+			this->button1->Location = System::Drawing::Point(58, 7);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 1;
@@ -104,20 +119,13 @@ namespace Project1 {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(72, 377);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(181, 20);
-			this->textBox1->TabIndex = 2;
-			// 
 			// chart2
 			// 
 			chartArea2->Name = L"ChartArea1";
 			this->chart2->ChartAreas->Add(chartArea2);
 			legend2->Name = L"Legend1";
 			this->chart2->Legends->Add(legend2);
-			this->chart2->Location = System::Drawing::Point(693, 2);
+			this->chart2->Location = System::Drawing::Point(610, 34);
 			this->chart2->Name = L"chart2";
 			series2->ChartArea = L"ChartArea1";
 			series2->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
@@ -126,23 +134,83 @@ namespace Project1 {
 			series2->Legend = L"Legend1";
 			series2->Name = L"Series1";
 			this->chart2->Series->Add(series2);
-			this->chart2->Size = System::Drawing::Size(658, 353);
+			this->chart2->Size = System::Drawing::Size(575, 287);
 			this->chart2->TabIndex = 3;
 			this->chart2->Text = L"chart2";
+			this->chart2->Click += gcnew System::EventHandler(this, &MyForm::chart2_Click);
+			// 
+			// chart3
+			// 
+			chartArea3->Name = L"ChartArea1";
+			this->chart3->ChartAreas->Add(chartArea3);
+			legend3->Name = L"Legend1";
+			this->chart3->Legends->Add(legend3);
+			this->chart3->Location = System::Drawing::Point(610, 340);
+			this->chart3->Name = L"chart3";
+			series3->ChartArea = L"ChartArea1";
+			series3->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
+			series3->Color = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			series3->Legend = L"Legend1";
+			series3->Name = L"Series1";
+			this->chart3->Series->Add(series3);
+			this->chart3->Size = System::Drawing::Size(575, 287);
+			this->chart3->TabIndex = 5;
+			this->chart3->Text = L"chart3";
+			// 
+			// chart4
+			// 
+			chartArea4->Name = L"ChartArea1";
+			this->chart4->ChartAreas->Add(chartArea4);
+			legend4->Name = L"Legend1";
+			this->chart4->Legends->Add(legend4);
+			this->chart4->Location = System::Drawing::Point(29, 340);
+			this->chart4->Name = L"chart4";
+			series4->ChartArea = L"ChartArea1";
+			series4->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
+			series4->Legend = L"Legend1";
+			series4->Name = L"Series1";
+			this->chart4->Series->Add(series4);
+			this->chart4->Size = System::Drawing::Size(575, 287);
+			this->chart4->TabIndex = 4;
+			this->chart4->Text = L"chart4";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(26, 17);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(26, 13);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"Y(n)";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(26, 324);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(26, 13);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"X(n)";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1364, 458);
+			this->ClientSize = System::Drawing::Size(1410, 788);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->chart3);
+			this->Controls->Add(this->chart4);
 			this->Controls->Add(this->chart2);
-			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->chart1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart4))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -165,21 +233,31 @@ namespace Project1 {
 		array<double>^ c = gcnew array<double>(500);
 		double aa;
 		int n, P, m, k;
-
+		int zapaz = 1; //запаздывание
 		y1 = randomis();
-		y2 = randomis();
-		for (n = 0; n < N; n++)
+		y2 = zapazd(y1, zapaz);
+		
+		for (int n = 0; n < N; n++)
 		{
 			y[n] = 2 * y1[n] + 5 * y2[n];
 		}
 
+		P = 2. / a;
+		N_realiz = N - 2 * P;
+			for (m = 0; m < 500; m++)
+			{
+				r[m] = 0.0;
+				for (n = 0; n < (N_realiz - m - 1); n++)
+					r[m] = r[m] + 1. / (N_realiz - m) * y[n] * y[n + m];
+				chart2->Series[0]->Points->AddXY(m, r[m]);
+			}
 
 			for (m = 0; m < 500; m++)
 			{
 				r[m] = 0.0;
 				for (n = 0; n < (N_realiz - m - 1); n++)
-					r[m] = r[m] + 1. / (N_realiz - m) * y[n] * x[n + m];
-				chart2->Series[0]->Points->AddXY(m, r[m]);
+					r[m] = r[m] + 1. / (N_realiz - m) * y1[n] * y1[n + m];
+				chart3->Series[0]->Points->AddXY(m, r[m]);
 			}
 
 		// Plot the probability density
@@ -187,8 +265,25 @@ namespace Project1 {
 			chart1->Series[0]->Points->AddXY(n, y[n]);
 			
 		}
-	}
 
+		for (int n = 0; n < N; n++) {
+			chart4->Series[0]->Points->AddXY(n, y1[n]);
+
+		}
+	}
+		   array<double>^ zapazd(array<double>^ y1, int zapaz)
+		   {
+			   array<double>^ y2 = gcnew array<double>(N);
+
+			   for (int n = 0; n < zapaz; n++) { y2[n] = 0; }
+
+			   for (int n = zapaz; n < N; n++)
+			   {
+				   y2[n] = y1[n - zapaz];
+			   }
+			   return y2;
+
+		   }
 		   float gauss(float mu, float sigma) {
 			   static float spare;
 			   static bool hasSpare = false;
@@ -257,5 +352,7 @@ namespace Project1 {
 			   return x;
 			   
 		   }
-	};
+	private: System::Void chart2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
